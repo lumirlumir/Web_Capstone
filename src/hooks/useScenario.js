668631385ba998d202dc28cf192d3b9ed759bb1f
LuMir <rpfos@naver.com>
2024-05-28 19:14:04 +0900

    refactor: create useScenario custom hook
    src/pages/Start.jsx에서 기존에 하드 코딩 되어있던 scenarioPhaseState와 관련 함수들을 하나로 묶어 useScenario custom hook으로 재정의.

---
+++ src/hooks/useScenario.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+import scenario from '@/data/scenario.json';
+
+const useScenario = () => {
+  /* Hooks */
+  // useState
+  const [scenarioPhaseState, setScenarioPhaseState] = useState({
+    major: 0,
+    minor: 0,
+  });
+
+  /* Func */
+  const handleNextScenarioPhaseState = type => {
+    const newScenarioPhaseState = scenarioPhaseState[type] + 1;
+
+    if (type === 'major' && newScenarioPhaseState < scenario.phase.length) {
+      setScenarioPhaseState(prevState => ({ ...prevState, major: newScenarioPhaseState, minor: 0 }));
+    } else if (type === 'minor' && newScenarioPhaseState < scenario.phase[scenarioPhaseState.major].length) {
+      setScenarioPhaseState(prevState => ({ ...prevState, [type]: newScenarioPhaseState }));
+    }
+  };
+  const handleSkipScenarioPhaseState = () => {
+    // for minor phase
+    const newScenarioPhaseState = scenario.phase[scenarioPhaseState.major].length - 1;
+
+    setScenarioPhaseState(prevState => ({ ...prevState, minor: newScenarioPhaseState }));
+  };
+  const isScenarioPhaseDone = () => {
+    // for minor phase
+    return scenarioPhaseState.minor === scenario.phase[scenarioPhaseState.major].length - 1;
+  };
+  const isTutorialPhase = () => {
+    return scenarioPhaseState.major === 0;
+  };
+  const isInterviewPhase = () => {
+    return scenarioPhaseState.major === 1;
+  };
+
+  /* Return */
+  return {
+    scenarioPhaseState,
+    handleNextScenarioPhaseState,
+    handleSkipScenarioPhaseState,
+    isScenarioPhaseDone,
+    isTutorialPhase,
+    isInterviewPhase,
+  };
+};
+
+export default useScenario;
+
+// TODO: use enum type to major phase.
--- src/pages/Start/Start.jsx
+++ src/pages/Start/Start.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import scenario from '@/data/scenario.json';
 import useConfig from '@/hooks/useConfig';
+import useScenario from '@/hooks/useScenario';
 
 import FooterL from './FooterL';
 import FooterM from './FooterM';
@@ -18,46 +19,8 @@ import './Start.scss';
  */
 function Start() {
   /* Hooks */
-  // useConfig
   const config = useConfig();
-  // scenarioPhase
-  const [scenarioPhaseState, setScenarioPhaseState] = useState({
-    major: 0,
-    minor: 0,
-  });
-  const handleNextScenarioPhaseState = type => {
-    const newScenarioPhaseState = scenarioPhaseState[type] + 1;
-
-    if (type === 'major' && newScenarioPhaseState < scenario.phase.length) {
-      setScenarioPhaseState(prevState => ({ ...prevState, major: newScenarioPhaseState, minor: 0 }));
-    } else if (type === 'minor' && newScenarioPhaseState < scenario.phase[scenarioPhaseState.major].length) {
-      setScenarioPhaseState(prevState => ({ ...prevState, [type]: newScenarioPhaseState }));
-    }
-  };
-  const handleSkipScenarioPhaseState = () => {
-    // for minor phase
-    const newScenarioPhaseState = scenario.phase[scenarioPhaseState.major].length - 1;
-
-    setScenarioPhaseState(prevState => ({ ...prevState, minor: newScenarioPhaseState }));
-  };
-  const isScenarioPhaseDone = () => {
-    // for minor phase
-    return scenarioPhaseState.minor === scenario.phase[scenarioPhaseState.major].length - 1;
-  };
-  const isTutorialPhase = () => {
-    return scenarioPhaseState.major === 0;
-  };
-  const isInterviewPhase = () => {
-    return scenarioPhaseState.major === 1;
-  };
-  const scenarioPhase = {
-    scenarioPhaseState,
-    handleNextScenarioPhaseState,
-    handleSkipScenarioPhaseState,
-    isScenarioPhaseDone,
-    isTutorialPhase,
-    isInterviewPhase,
-  };
+  const scenarioPhase = useScenario();
 
   // useQuestionMain
   // const [arrayCsQuestionMain, generateCsQuestionMain] = useQuestionMain('cs', 3);

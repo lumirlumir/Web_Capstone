9aee7b6c58c2f4500b4ebc6568994d5e882f931e
LuMir <rpfos@naver.com>
2024-06-04 00:59:03 +0900

    fix: SectionServer
    SectionServer.jsx의 history 관리하는 부분 오류 수정.

--- src/pages/Start/Main/SectionServer/SectionServer.jsx
+++ src/pages/Start/Main/SectionServer/SectionServer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
+import React, { useState, useRef, useLayoutEffect, useMemo } from 'react';
 import Typewriter from 'typewriter-effect';
 
 import CompDivNeon from '@/components/CompDivNeon';
@@ -12,34 +12,43 @@ import './SectionServer.scss';
  */
 function SectionServer({ scenario, config, interview }) {
   /* Props */
-  const { isSection, subsectionState, getSubsectionObj, toNextSubsection } = scenario;
+  const { subsectionState, getSubsectionObj, toNextSubsection } = scenario;
   const { auto, api } = getSubsectionObj().global;
   const { visibility, content } = getSubsectionObj().Main.SectionServer;
   const { configState } = config;
   const { getQuestionMain } = interview;
 
   /* Hooks */
-  // useState
-  const [contentHistoryState, setContentHistoryState] = useState('');
-  // useRef
+  // useHistory
+  const [prevHistoryState, setPrevHistoryState] = useState('');
+  const [, setHistoryState] = useState('');
+
+  const handleHistoryState = text => {
+    setHistoryState(prevState => {
+      setPrevHistoryState(prevState);
+      return `${prevState}${text}`;
+    });
+  };
+  // useScroll
   const scrollRef = useRef();
+  const scroll = () => {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
+  };
   // useMemo
   const text = useMemo(() => {
     if (api) return `> ${getQuestionMain()}\n\n`;
 
     return content;
   }, [api, content, getQuestionMain]);
-  // useEffect
-  useEffect(() => {
-    if ((isSection('tutorial') && subsectionState > 1) || isSection('interview')) {
-      scrollRef.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' });
-    }
-  }, [subsectionState, isSection, getQuestionMain]);
+  // useLayoutEffect
+  useLayoutEffect(() => {
+    handleHistoryState(text);
+  }, [subsectionState, text]);
 
   /* Return */
   return (
     <CompDivNeon className={`SectionServer ${visibility && !configState.visibility ? '' : 'invisible'}`} neonColor="black">
-      <div>{contentHistoryState}</div>
+      <div>{prevHistoryState}</div>
       <div>
         <Typewriter
           key={text}
@@ -54,8 +63,8 @@ function SectionServer({ scenario, config, interview }) {
               .pauseFor(1000) // original: 1000
               .start()
               .callFunction(() => {
-                setContentHistoryState(`${contentHistoryState}${text}`);
                 if (auto) toNextSubsection();
+                scroll();
               });
           }}
         />

9b1a5ebe26e6ada1d5dcb2d13977841c048d60f9
LuMir <rpfos@naver.com>
2024-06-05 14:33:19 +0900

    refactor: wrap methods in useScenario with useCallback

--- src/hooks/scenario/useScenario.js
+++ src/hooks/scenario/useScenario.js
@@ -1,23 +1,21 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import scenario from '@/data/scenario.json';
 
+/* Constants */
+const { TUTORIAL, INTERVIEW, RESULT } = Object.freeze({
+  TUTORIAL: 0,
+  INTERVIEW: 1,
+  RESULT: 2,
+});
+const { chapter: CHAPTER } = Object.freeze(scenario);
+
 /**
- * scenario > chapter > section > subsection => scenario.chapter[section][subsection]
+ * scenario > chapter > section > subsection => scenario[chapter][section][subsection]
  *
  * @returns
  */
 const useScenario = () => {
-  /* Variables */
-  const { chapter } = scenario;
-
-  /* Constants */
-  const { TUTORIAL, INTERVIEW, RESULT } = Object.freeze({
-    TUTORIAL: 0,
-    INTERVIEW: 1,
-    RESULT: 2,
-  });
-
   /* Hooks */
   // useState
   const [state, setState] = useState({
@@ -25,34 +23,37 @@ const useScenario = () => {
     subsection: 0,
   });
 
-  /* Func Public */
+  /* Func */
   // section
-  const isSection = sectionType => {
-    switch (sectionType.toUpperCase()) {
-      case 'TUTORIAL':
-        return state.section === TUTORIAL;
-      case 'INTERVIEW':
-        return state.section === INTERVIEW;
-      case 'RESULT':
-        return state.section === RESULT;
-      default:
-        return null;
-    }
-  };
+  const isSection = useCallback(
+    sectionType => {
+      switch (sectionType.toUpperCase()) {
+        case 'TUTORIAL':
+          return state.section === TUTORIAL;
+        case 'INTERVIEW':
+          return state.section === INTERVIEW;
+        case 'RESULT':
+          return state.section === RESULT;
+        default:
+          return null;
+      }
+    },
+    [state],
+  );
   // subsection
-  const getSubsectionObj = () => chapter[state.section][state.subsection];
-  const toNextSubsection = () => {
+  const getSubsectionObj = useCallback(() => CHAPTER[state.section][state.subsection], [state]);
+  const toNextSubsection = useCallback(() => {
     setState(prevState => {
       const newSectionState = state.section + 1;
       const newSubsectionState = state.subsection + 1;
 
-      if (newSubsectionState < chapter[state.section].length) {
+      if (newSubsectionState < CHAPTER[state.section].length) {
         return {
           ...prevState,
           subsection: newSubsectionState,
         };
       }
-      if (newSectionState < chapter.length) {
+      if (newSectionState < CHAPTER.length) {
         return {
           ...prevState,
           section: newSectionState,
@@ -61,17 +62,17 @@ const useScenario = () => {
       }
       return prevState;
     });
-  };
-  const toLastSubsection = () => {
+  }, [state]);
+  const toLastSubsection = useCallback(() => {
     setState(prevState => {
-      const newSubsectionState = chapter[state.section].length - 1;
+      const newSubsectionState = CHAPTER[state.section].length - 1;
 
       return {
         ...prevState,
         subsection: newSubsectionState,
       };
     });
-  };
+  }, [state]);
 
   /* Return */
   return {

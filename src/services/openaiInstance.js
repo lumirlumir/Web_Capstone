572fba2ea4d2657e4396030d698055b77a4aee50
LuMir <rpfos@naver.com>
2024-06-09 17:52:13 +0900

    refactor: back-end introduction and modification of fetch instructions
    보안 문제로 백엔드를 도입하여, 기존에 openai에서 직접 JSON 데이터를 받아오던 방식을 백엔드 서버에서 받아오는 방식으로 수정.
    위 내용과 관련된 openaiService.js 내부의 fetch 함수들 일괄 수정.
    github workflow도 수정하여 자동 배포 시 서버의 주소와 포트 번호를 받아올 수 있도록 설정.

--- src/services/openaiInstance.js
+++
@@ -1,8 +0,0 @@
-import OpenAI from 'openai';
-
-const openaiInstance = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-
-export default openaiInstance;
--- src/services/openaiService.js
+++ src/services/openaiService.js
@@ -1,55 +1,11 @@
-import openaiJson from '@/data/openai.json';
-import openaiInstance from '@/services/openaiInstance';
-import { createMessagesObject, stringArrayToMessagesObjectArray } from '@/utils/openaiUtils';
+import qs from 'qs';
 
-const fetch = async messages => {
-  const response = await openaiInstance.chat.completions.create({
-    ...openaiJson.requestBody,
-    messages,
-  });
+const url = (path, obj) => `http://${process.env.BACKEND_IP}:${process.env.BACKEND_PORT}/${path}?${qs.stringify(obj)}`;
 
-  return response?.choices?.[0]?.message?.content;
-};
+export const fetchQuestionMain = async (type, history) => (await (await fetch(url('question/main', { type, history }))).json())?.text;
 
-/**
- *
- * @param {string} type select among 'cs', 'fe', 'be', 'db', 'oop'
- * @param {string[]} history
- * @returns
- */
-export const fetchQuestionMain = async (type, history) => fetch([...openaiJson.fetchQuestionMain[type.toLowerCase()].messages, ...stringArrayToMessagesObjectArray('assistant', history)]);
+export const fetchQuestionSub = async (question, answerUser) => (await (await fetch(url('question/sub', { question, answerUser }))).json())?.text;
 
-/**
- *
- * @param {string} question
- * @param {string} answerUser
- * @returns
- */
-export const fetchQuestionSub = async (question, answerUser) => fetch([...openaiJson.fetchQuestionSub.messages, createMessagesObject('user', `Previous Question\n\n${question}\n\nUSER's Answer\n\n${answerUser}`)]);
+export const fetchAnswer = async question => (await (await fetch(url('answer', { question }))).json())?.text;
 
-/**
- *
- * @param {string} question
- * @returns
- */
-export const fetchAnswer = async question => fetch([...openaiJson.fetchAnswer.messages, createMessagesObject('user', question)]);
-
-/**
- *
- * @param {string} answerSystem
- * @param {string} answerUser
- * @returns
- */
-export const fetchFeedback = async (answerSystem, answerUser) => fetch([...openaiJson.fetchFeedback.messages, createMessagesObject('user', `Correct Answer\n\n${answerSystem}\n\nUSER's Answer\n\n${answerUser}`)]);
-
-/*
-TODO: make enum type and adjust openai.json
-
-ex)
-
-const generateType = {
-  fetchQuestionMain: 1
-  generateAnswer: 2
-  //...
-}
-*/
+export const fetchFeedback = async (answerSystem, answerUser) => (await (await fetch(url('feedback', { answerSystem, answerUser }))).json())?.text;
